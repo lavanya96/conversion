ee1969daece60b57bd57dae827a1327a89dc678f
lavanya96 <lavanyaksr92@gmail.com>
2017-06-09 14:28:57 +0530

    Create converse.java

---
+++ converse.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class converse
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in)
+String a=s.nextLine();
+int b=Integer.parseInt(a);
+System.out.println(b);
+}
+}
